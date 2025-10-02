d97b8b9110937db9b101293f9c05d691c31166b9
박현수 <zgustn0@gmail.com>
2025-10-02 14:51:23 +0900

    feat:team DTO 추가

--- src/main/java/org/example/domain/Team.java
+++ src/main/java/org/example/domain/Team.java
@@ -1,4 +1,29 @@
 package org.example.domain;
 
+
+//팀은 팀명,소속지역,인원수로 나뉜다.
 public class Team {
+
+    private String teamName;
+    private String location;
+    private int playerNums;
+
+
+    public Team(String teamName, String location, int playerNums) {
+        this.teamName = teamName;
+        this.location = location;
+        this.playerNums = playerNums;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getPlayerNums() {
+        return playerNums;
+    }
 }
