a6843c5f57155c6da5186ce3982d8a3d881ace0c
박현수 <zgustn0@gmail.com>
2025-10-04 04:52:01 +0900

    feat:team 서비스 추가

---
+++ src/main/java/org/example/service/TeamService.java
@@ -0,0 +1,28 @@
+package org.example.service;
+
+import org.example.dao.TeamDAO;
+import org.example.dao.jdbc.JdbcTeamDao;
+import org.example.domain.Team;
+
+import java.util.List;
+
+public class TeamService {
+
+    TeamDAO teamDao = new JdbcTeamDao();
+
+    public int save(Team team){
+        return teamDao.insertTeam(team);
+    }
+    public int delete(Team team){
+        return teamDao.deleteTeam(team);
+    }
+    public Team find(int id){
+        return teamDao.selectTeam(id);
+    }
+    public List<Team> findAll(){
+        return teamDao.selectAll();
+    }
+
+
+
+}
